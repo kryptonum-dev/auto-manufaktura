1f31eb245cd7645cdb7dcac2b52c23bbf48e4f71
Marta Zaorska <marta.zaorska2@gmail.com>
2024-12-02 17:08:36 +0100

    added logo and cta button to Header

--- next/src/components/global/Header/_Header.tsx
+++ next/src/components/global/Header/_Header.tsx
@@ -1,5 +1,29 @@
+import Link from 'next/link';
+import Button from '@/components/ui/Button';
 import type { HeaderPropsTypes } from './Header.types';
+import styles from './Header.module.scss';
 
 export default function Header({ logo }: HeaderPropsTypes) {
-  return <>{logo}</>;
+  return (
+    <>
+      <header className={styles['Header']}>
+        <div className='max-width'>
+          <Link
+            href='/'
+            aria-label='Przejdź do strony głównej'
+            className={styles.logo}
+          >
+            {logo}
+          </Link>
+          <Button
+            href='/'
+            text='Kontakt'
+            theme='primary'
+            linkType='internal'
+            className={styles.cta}
+          />
+        </div>
+      </header>
+    </>
+  );
 }
