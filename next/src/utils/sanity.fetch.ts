4aa67ad344b80f7759417f7ca98b30c297c74ac8
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2025-01-29 12:10:54 +0100

    try to fix timed out during build

--- next/src/utils/sanity.fetch.ts
+++ next/src/utils/sanity.fetch.ts
@@ -3,19 +3,17 @@ import { createClient, type QueryParams } from 'next-sanity';
 import { isPreviewDeployment, isProductionDeployment } from './is-preview-deployment';
 
 const projectId = process.env.SANITY_PROJECT_ID;
-const token = process.env.SANITY_API_TOKEN;
+const TOKEN = process.env.SANITY_API_TOKEN;
 
-if (!token) {
-  throw new Error('The `SANITY_API_TOKEN` environment variable is required.');
-}
+if (!TOKEN) throw new Error('The `SANITY_API_TOKEN` environment variable is required');
 
 const client = createClient({
-  projectId,
+  projectId: projectId,
   dataset: 'production',
   apiVersion: '2024-11-28',
   useCdn: false,
   perspective: isPreviewDeployment ? 'previewDrafts' : 'published',
-  token,
+  token: TOKEN,
 });
 
 /**
@@ -36,17 +34,11 @@ export default async function sanityFetch<QueryResponse>({
 }): Promise<QueryResponse> {
   return await client.fetch<QueryResponse>(query, params, {
     ...(!isProductionDeployment
-      ? {
-          cache: 'reload',
-        }
+      ? { cache: 'reload' }
       : {
-          ...(isPreviewDeployment || !tags
-            ? {
-                cache: 'no-cache',
-              }
-            : {
-                next: { tags },
-              }),
-        }),
+        ...(isPreviewDeployment || !tags)
+          ? { cache: 'no-cache' }
+          : { next: { tags } },
+      }),
   });
 }
